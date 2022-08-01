32dd20b5518179478bf2ab41d23baf534d902a4d
Iv Robinson <arobinsoniv@mitre.org>
2022-08-01 09:46:45 -0400

    Remove relative path in bl_gdb.sh

--- tools/bl_gdb.sh
+++ tools/bl_gdb.sh
@@ -2,7 +2,7 @@
 
 if printf -v regex ':%04X .* 0A ' 1234;grep -q "$regex" /proc/net/tcp*;
 then
-    gdb-multiarch -ex 'target remote :1234' -ex 'layout split' -ex 'file /home/jovyan/insecure_example/bootloader/gcc/main.axf'
+    gdb-multiarch -ex 'target remote :1234' -ex 'layout split' -ex 'file ../bootloader/gcc/main.axf'
 else
     echo "Didn't detect GDB port listening. Did you remember to run python ./bl_emulate --debug ?"
 fi
