927535e74ca50622a61ad0829f42bb02d32c20f0
triantatria <connoredelheit@gmail.com>
2023-07-26 19:15:00 +0000

    bl changes

--- tools/eccKeygen.py
+++ tools/eccKeygen.py
@@ -3,9 +3,13 @@ from Crypto.PublicKey import ECC
 key = ECC.generate(curve='P-256')
 f = open('secret_build_output.txt','a')
 #pub = open('secret_build_output.txt','wt')
-f.write(key.export_key(format='PEM'))
+
+eccPrivKey = key.export_key(format='PEM')
+f.write(eccPrivKey)
 f.write("\n")
-f.write(key.public_key().export_key(format='PEM'))
+eccPubKey = key.public_key().export_key(format='PEM')
+f.write(eccPubKey)
+#f.write(key.public_key().export_key(format='PEM'))
 #f.close()
 
 #f = open('secret_build_output.txt','rt')
