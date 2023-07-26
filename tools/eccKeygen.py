b8f967042edfff21a0cc9f616b5fcba632dfc4d3
triantatria <connoredelheit@gmail.com>
2023-07-26 15:46:08 +0000

    added eccKeygen

---
+++ tools/eccKeygen.py
@@ -0,0 +1,12 @@
+from Crypto.PublicKey import ECC
+
+key = ECC.generate(curve='P-256')
+f = open('secret_build_output.txt','wt')
+#pub = open('secret_build_output.txt','wt')
+f.write(key.export_key(format='PEM'))
+f.write("\n")
+#f.close()
+
+#f = open('secret_build_output.txt','rt')
+f.write(key.public_key().export_key(format='PEM'))
+#privkey = ECC.import_key(f.read())
