ace35781f5316917a7162d0774ef98107c21dfeb
Fredswqa190 <72878800+Fredswqa190@users.noreply.github.com>
2023-08-02 22:22:51 -0400

    updated the failed gitignore because i cant read

--- bootloader/src/secrets.h
+++ bootloader/src/secrets.h
@@ -3,7 +3,7 @@
 #define SECRETS_H
 const uint8_t AES_KEY[16] = {};
 const uint8_t IV[16] = {};
-const uint8_t CHA_KEY[32] = {2};
+const uint8_t CHA_KEY[32] = {};
 const uint8_t NONCE[12] = {};
 const uint8_t AAD[26] = {};
 #endif
